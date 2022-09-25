be82c245f1431ad3352365e0a56c8240aa2eced0
Tomislav Busic <tomislav.busic89@gmail.com>
2022-09-25 16:45:14 +0200

    options

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const menuBtn = document.getElementById('menu_btn_id');
 const menu = document.querySelector('.menu ul');
 
-const homeChoiceSale = document.getElementById('sale_id');
-const homeChoiceRent = document.getElementById('rent_id');
+let sectionForSale = document.getElementById('sale_section_id');
+
+let input = document.getElementById('input_search_id');
+let optionsForSale = document.getElementById('options_id');
+let items = [];
+
 
 //Copyright and Date
 let today = new Date;
@@ -25,13 +29,142 @@ window.addEventListener("scroll", () => {
     menu.style.right = "-15rem";
 });
 
-//Add event listeners for the home choices
-homeChoiceRent?.addEventListener("click", () => {
-    window.location.href = "rent.html";
+//Fetching Data
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
+		'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+	}
+};
+
+async function fetchData(){
+    try{
+        const response = await fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=200&sort=relevance', options)
+        const data = await response.json();
+        items = data.listings; 
+        DisplayForSale(data.listings);   
+        console.log(data.listings);
+    } catch(e){
+        console.log(e)
+    }
+} 
+
+//Search by City name
+input.addEventListener("input", (e) => {
+    let value = e.target.value.toLowerCase();
+
+    const filtered = items.filter( item =>
+        item.address_new.city.toLowerCase().includes(value)   
+    );
+
+    DisplayForSale(filtered);
 });
 
-homeChoiceSale?.addEventListener("click", () => {
-    window.location.href = "sale.html";
-})
+//Sorting by criteria
+optionsForSale.addEventListener('change', (e) => {
+    let value = e.target.value;
+
+    switch(value) {
+        case 'All':
+            DisplayForSale(items);
+            break;
+        case 'lower_price':
+            let filteredByLower = items.sort((a,b) => 
+            a.price_raw - b.price_raw);
+            DisplayForSale(filteredByLower);
+            break;
+        case 'higher_price':
+            let filteredByHigher = items.sort((a,b) => 
+            b.price_raw - a.price_raw);
+            DisplayForSale(filteredByHigher);
+            break;  
+        case 'newly_built':
+            let filteredByBuilt = items.filter( item =>
+            item.is_new_construction);
+            DisplayForSale(filteredByBuilt);
+            break; 
+        case 'condo':
+            let filteredByCondo = items.filter( item =>
+            item.prop_type === 'condo');
+            DisplayForSale(filteredByCondo);
+            break;
+        case 'multi_family':
+            let filteredBySinFem = items.filter( item =>
+            item.prop_type === 'multi_family');
+            DisplayForSale(filteredBySinFem);
+            break;
+        case 'single_family':
+            let filteredByMulFem = items.filter( item =>
+            item.prop_type === 'single_family');
+            DisplayForSale(filteredByMulFem);
+            break;      
+    } 
+});
+
+//Type of property for each item
+const propType = (type) => {
+    switch(type) {
+        case 'condo':
+            type = 'Condo';
+            break;
+        case 'multi_family':
+            type = 'Multifamily';
+            break;
+        case 'single_family':
+            type = 'Single Family';
+            break;
+    }
+    return type;
+}
+
+//Replace data in correct place
+const DisplayForSale = (data) => {
+     let products = data?.map((item) => { 
+
+                 template_sale_data = template_sale_data_id.innerHTML;
+
+                 template_sale_data = template_sale_data.replaceAll('${primary_photo}', item?.['photo'] ? item['photo'] : 'https://image.shutterstock.com/image-vector/illustration-simple-house-isolated-on-260nw-1937900650.jpg');
+                 template_sale_data = template_sale_data.replaceAll('${branding}', item['office_name']);
+                 template_sale_data = template_sale_data.replaceAll('${status}', item['prop_status'] === 'for_sale' ? 'For Sale' : '');
+                 template_sale_data = template_sale_data.replaceAll('${state}', item['address_new']['state']);
+                 template_sale_data = template_sale_data.replaceAll('${city}', item['address_new']['city']);
+                 template_sale_data = template_sale_data.replaceAll('${address}', item['address_new']['line']);
+                 template_sale_data = template_sale_data.replaceAll('${postal_code}', item['address_new']['postal_code']);
+                 template_sale_data = template_sale_data.replaceAll('${is_new_construction}', !item['is_new_construction'] ? 'No' : 'Yes');
+                 template_sale_data = template_sale_data.replaceAll('${prop_type}', propType(item['prop_type']));
+                 template_sale_data = template_sale_data.replaceAll('${baths}', item['baths']);
+                 template_sale_data = template_sale_data.replaceAll('${beds}', item['beds']);
+                 template_sale_data = template_sale_data.replaceAll('${price}', item['price']); 
+                
+                return template_sale_data;
+                
+     }).join('');
+
+     //Insert template into sale section
+     sectionForSale.innerHTML = products;
+} 
+
+//initalization
+fetchData();
 
 
+
+
+
+
+
+/* 
+Detail
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
+		'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+	}
+};
+
+fetch('https://realty-in-us.p.rapidapi.com/properties/detail?listing_id=608763437&prop_status=for_sale&property_id=4599450556', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err)); */
